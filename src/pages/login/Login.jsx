e2423ebd5f0771062ddf15539ef1039c1ec98c27
DevThomas <fremen1990@gmail.com>
2022-06-28 14:09:50 +0200

    AuthContext created

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,18 @@
 import "./login.css";
+import {useRef} from "react";
 
 export default function Login() {
+
+
+  const email = useRef();
+  const password = useRef();
+
+  const handleClick = (e)=>{
+    e.preventDefault()
+    console.log(email.current.value, password.current.value)
+  }
+
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +23,15 @@ export default function Login() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" className="loginInput" ref={email} required minLength="6"/>
+            <input placeholder="Password" type="password" className="loginInput" ref={password} required minLength="6"/>
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
