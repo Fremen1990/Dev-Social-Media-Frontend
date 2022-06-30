a4cb8f80e00a945b928285b5b7a62714e01cb0d7
DevThomas <fremen1990@gmail.com>
2022-06-30 12:47:53 +0200

    autoloing disabled

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,26 +2,7 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: {
-    _id: "62bc5f0cea0b7b7047c98a42",
-    username: "elonmusk",
-    email: "ElonMarsGuy@tesla.com",
-    profilePicture: "person/elon-musk.webp",
-    coverPicture: "",
-    followers: ["62bc5f8dea0b7b7047c98a4b"],
-    followings: [
-      "62bc5f8dea0b7b7047c98a4b",
-      "62bc5f26ea0b7b7047c98a44",
-      "62bc5f3fea0b7b7047c98a46",
-    ],
-    isAdmin: false,
-    createdAt: "2022-06-29T14:17:48.188Z",
-    __v: 0,
-    city: "Philadelphia",
-    firstName: "Elon",
-    from: "Africa",
-    lastName: "Musk",
-  },
+  user: {},
   isFetching: false,
   error: false,
 };
